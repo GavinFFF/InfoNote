25c59e93631f20049c157f2318161996aeb4724b
guanfei <gavinfei25@gmail.com>
2018-05-27 16:07:14 +0800

    info包，业务逻辑根目录，业务逻辑独立

--- manager.py
+++ manager.py
@@ -1,28 +1,6 @@
-import redis
-
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_session import Session
-from flask_wtf import CSRFProtect
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from config import Config
-
-app = Flask(__name__)
-
-app.config.from_object(Config)
-
-# 数据库
-db = SQLAlchemy(app)
-
-# redis
-redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-
-# 开启CSRF
-CSRFProtect(app)
-
-# 指定session保存位置
-Session(app)
+from info import app, db
 
 # manager启动程序
 manager = Manager(app)
